032c7a002e72b981e6a55c4736581645719600c0
xieminhui <xieminhui@corp.netease.com>
2018-08-31 17:48:38 +0800

    file api

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2016,
+        "sourceType": "module"
+    },
+    "globals": {
+        __dirname: false,
+        process: false
+    },
+    "rules": {
+        "indent": 0,
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": [
+            'warn'
+        ],
+        "no-console": 0
+    }
+};
---
+++ file api/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+// const { URL } = require('url');
+// const fileUrl = new URL('file://file/test_file.txt');
+const url = 'nodejs/file/test_file.txt';
+let fdNum;
+let strBuff = new Buffer('雷军说，');
+fs.open(url, 'r+', (err, fd) =>{
+   if(err){
+      console.error(err);
+   }
+   console.log(fd);
+   fdNum = fd;
+   fs.write(fdNum, strBuff, 0, strBuff.length, 0);
+   fs.fstat(fd, (err, stat) => {
+      if(err)console.log(err);
+      console.log(stat);
+   });
+});
+fs.readFile(url,'utf8', (err, data) =>{
+   if(err)console.error(err);
+   console.log(data);
+});
+fs.watchFile(url, (curr, prev) => {
+   console.log(curr);
+   console.log(prev);
+});
