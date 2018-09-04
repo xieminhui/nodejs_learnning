6ada8234b282d36be6656daec758f5e3fcb4e6a4
xieminhui <xmh19940228@163.com>
2018-09-04 10:45:28 +0800

    nodejs learnning ---- readline

--- readline/index.js
+++ readline/index.js
@@ -2,9 +2,21 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
    input: process.stdin,
-   output: process.stdout
+   output: process.stdout,
+   prompt: '请输入>'
 });
 
 rl.on('line', (line) => {
-   console.log(`接收到${line}`);
+   switch(line.trim()){
+      case 'hello':
+         console.log('world');
+         break;
+      default:
+         console.log(`你输入的是${line.trim()}`);
+         break;
+   }
+   rl.prompt();
+}).on('close', () => {
+   console.log('goodbye');
+   process.exit(0);
 });
